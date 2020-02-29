2088959363dbe5fb0ee31f56960f9d3a80bcf954
bbecomp99 <43157160+bbecomp99@users.noreply.github.com>
2020-02-29 12:41:30 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "${file("Fox Infra-3668afdf9e8f.json")}"
+ credentials = "${file("EEEo*****f.json")}"
  project     = "fox-infra"
  region      = "us-central-a"
 }
